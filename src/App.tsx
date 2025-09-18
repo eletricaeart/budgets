3ff6ed062a5c6d3f3749ab26f11db95358ed198d
Ceo-Sammarco <noah.kd@gmail.com>
2025-09-18 12:08:48 -0300

    @Ceo-Sammarco - 18/09/2025

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 
 import '../src/styles/globals.css';
 import OrcamentoBase from '../src/components/OrcamentoBase.tsx';
-
+import BudgetForm from './components/ea/budgetForm/BudgetForm.tsx';
 
 export default function App() {
 
-  return <OrcamentoBase />;
+  return( <>
+    <OrcamentoBase />
+    <BudgetForm />
+  </> );
 }
 
